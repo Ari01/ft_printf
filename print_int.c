103245f97affac089a222f8958636ea065607496
denden <denis.c1@live.fr>
2020-12-04 12:23:40 +0100

    correction nb d'espaces print 0 with prec0

--- print_int.c
+++ print_int.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 08:02:11 by user42            #+#    #+#             */
-/*   Updated: 2020/12/04 12:09:59 by user42           ###   ########.fr       */
+/*   Updated: 2020/12/04 12:23:05 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ size_t			get_nspaces(t_spec spec, char *arg, int nzero)
 	int nblen;
 
 	nblen = ft_strlen(arg);
-	if (!*arg && !spec.precision)
+	if (!ft_atoi(arg) && !spec.precision)
 		nblen--;
 	if (spec.width > nzero + nblen)
 		return (spec.width - nblen - nzero);
