63f2108d2c8aa3bcf9dc7f8c95d545613bc44141
denden <denis.c1@live.fr>
2020-12-05 10:21:14 +0100

    correction print p pour prendre en compte le flag 0

--- print_adress.c
+++ print_adress.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 17:00:38 by user42            #+#    #+#             */
-/*   Updated: 2020/12/04 16:14:55 by user42           ###   ########.fr       */
+/*   Updated: 2020/12/05 10:20:49 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,33 +20,61 @@ static void	freeptrs(char *s, char *space)
 	space = NULL;
 }
 
+int			set_space_zero(t_spec spec, char **space, char **zero, int slen)
+{
+	int size;
+
+	size = spec.width - slen;
+	if (size > 0)
+	{
+		if (spec.zero > 0)
+		{
+			*zero = ft_strnew('0', size);
+			if (!*zero)
+				return (0);
+		}
+		else
+		{
+			*space = ft_strnew(' ', size);
+			if (!*space)
+				return (0);
+		}
+	}
+	return (1);
+}
+
+static int	print_all(t_spec spec, char *s, char *zero, char *space)
+{
+	int nbytes_written;
+
+	nbytes_written = 0;
+	if (space && spec.minus < 0)
+		nbytes_written += strlprint(space, ft_strlen(space));
+	if (zero)
+		nbytes_written += strlprint(space, ft_strlen(zero));
+	nbytes_written += strlprint(s, ft_strlen(s));
+	if (space && spec.minus > 0)
+		nbytes_written += strlprint(space, ft_strlen(space));
+	return (nbytes_written);
+}
+
 int			print_adress(t_spec spec, void *adr)
 {
-	unsigned long int	n;
 	char				*s;
 	char				*space;
-	int					nspace;
+	char				*zero;
+	int					nbytes_written;
 	int					slen;
 
-	nspace = 0;
 	space = NULL;
-	n = (unsigned long int)adr;
-	s = ft_strjoin("0x", convert_base(n, "0123456789abcdef"));
+	zero = NULL;
+	s = ft_strjoin("0x", convert_base((unsigned long int)adr, "0123456789abcdef"));
 	if (!s)
 		return (0);
 	slen = ft_strlen(s);
-	if (spec.width > slen)
-	{
-		nspace = spec.width - ft_strlen(s);
-		space = ft_strnew(' ', nspace);
-		if (!space)
-			return (0);
-	}
-	if (space && spec.minus < 0)
-		ft_putstr_fd(space, STDOUT_FILENO);
-	ft_putstr_fd(s, STDOUT_FILENO);
-	if (space && spec.minus > 0)
-		ft_putstr_fd(space, STDOUT_FILENO);
+	if (!set_space_zero(spec, &space, &zero, slen))
+		return (0);
+	nbytes_written = print_all(spec, s, zero, space);
 	freeptrs(s, space);
-	return (slen + nspace);
+	return (nbytes_written);
 }
